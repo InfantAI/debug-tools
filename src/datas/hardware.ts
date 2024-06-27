8d0b159ec2cf6b65be8b19b0c8c64f80816679cc
ngai.wave <94841368+ngaiwave@users.noreply.github.com>
2024-06-27 13:35:34 +0800

    Update hardware.ts

--- src/datas/hardware.ts
+++ src/datas/hardware.ts
@@ -47,9 +47,9 @@ export const pushFunctions = [
     },
     {
       FunctionName: "固件升级",
-      typeflag: 3, // 假设固件升级的typeflag是3
+      typeflag: 1, // 假设固件升级的typeflag是3
       jsonData: {
-        url: "https://gadget-oss.oss-ap-southeast-5.aliyuncs.com/instaanswer/test/file/1702200014467.mp3",
+        url: "https://gadget-oss.oss-ap-southeast-5.aliyuncs.com/instaanswer/test/file/1805884136120909824.img",
         version: "1.0.1"
       },
       MQType: "devicedoll_msg_public",
